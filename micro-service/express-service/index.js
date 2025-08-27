ee2d8c777355958560eff7eeb81921de23df5eba
Rzq_0 <rizkywirajaya946@gmail.com>
2025-08-27 10:17:36 +0800

    add: menambah implementasi rabbitmq di express service

--- micro-service/express-service/index.js
+++ micro-service/express-service/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const api = require('./src/routes/api.js')
+const { initRabbitMq } = require('./src/utils/initRabbitMq.js')
+const startWorker = require('./src/workers')
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res)=> {
-    res.send("hello world testing ok sip");
-})
-app.get('/hello', (req,res)=> {
-    res.send("testing");
-})
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.listen(port, () =>{
-    console.log(`server is running on http://localhost:${port}`)
-})  
+app.use('/v1', api )
+
+
+async function startServer(){
+    try {
+        await initRabbitMq()
+        startWorker()
+
+        app.listen(port, () =>{
+            console.log(`server is running on http://localhost:${port}`)
+        })
+    } catch (error) {
+        console.error('Error starting server:', error)
+        process.exit(1)
+    }
+}
+startServer()
