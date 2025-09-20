6ce810f39da841508626e8881201896f36ecb529
Rzq_0 <rizkywirajaya946@gmail.com>
2025-09-21 06:33:33 +0800

    add: product service

--- micro-service/laravel-service/routes/api/product/product.php
+++ micro-service/laravel-service/routes/api/product/product.php
@@ -1,13 +1,13 @@
-<?php
+// <?php
 
-use App\Http\Controllers\Product\ProductController;
-use App\Http\Middleware\authUser;
-use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\Product\ProductController;
+// use App\Http\Middleware\authUser;
+// use Illuminate\Support\Facades\Route;
 
 
-Route::middleware([authUser::class])->group(function () {
-    Route::get('/products', [ProductController::class, 'index']);
-    Route::get('/products/{id}', [ProductController::class, 'show']);
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-});
+// Route::middleware([authUser::class])->group(function () {
+//     Route::get('/products', [ProductController::class, 'index']);
+//     Route::get('/products/{id}', [ProductController::class, 'show']);
+//     Route::post('/products', [ProductController::class, 'store']);
+//     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+// });
